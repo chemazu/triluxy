27a412b212116040556147b904e1884a558d966e
chemazu <chemazu>
2022-03-11 13:19:43 +0100

    search new

--- src/view/Search/index.jsx
+++ src/view/Search/index.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
+import data from "../../resource/hotel.json";
+import useSearch from "../../hooks/search-hook";
+import { SearchContext } from "../../context/SearchContext";
+
 import "./style.scss";
+import Banner from "../../components/Banner";
 
 export default function Search() {
-  console.log("srarc");
-  return <div className="searchResults">index</div>;
+  const context = useContext(SearchContext);
+  const { searchResult } = context;
+  const { result, length } = useSearch(searchResult, data);
+  console.log(result);
+  return (
+    <div className="searchResults">
+      <div className="desktop">
+        <div className="left">
+          <div className="one">
+            <span>Home {"> "} </span>
+            <span>
+              {searchResult.location} {"> "}
+            </span>
+            <span>Search result : </span>
+          </div>
+          <div className="two"></div>
+          <div className="three"></div>
+        </div>
+
+        <div className="right">
+          <div className="top">
+            <p>
+              {searchResult.location}: {length} properties found
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
