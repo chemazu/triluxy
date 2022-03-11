8914b72088f4ebbc00e8e1b452bcc04454d6ecc3
chemazu <chemazu>
2022-03-11 15:00:42 +0100

    friday

---
+++ src/resource/index.jsx
@@ -0,0 +1,9 @@
+import sort from "./sort.svg";
+import data from "./hotel.json";
+
+export default function ImportContent() {
+  return {
+    sort,
+    data,
+  };
+}
--- src/view/Search/index.jsx
+++ src/view/Search/index.jsx
@@ -1,16 +1,19 @@
-import React, { useContext } from "react";
-import data from "../../resource/hotel.json";
+import React, { useContext, useState } from "react";
 import useSearch from "../../hooks/search-hook";
 import { SearchContext } from "../../context/SearchContext";
-
 import "./style.scss";
-import Banner from "../../components/Banner";
+import ImportContent from "../../resource";
 
 export default function Search() {
+  const [show, setShow] = useState(false);
+  const { sort, data } = ImportContent();
   const context = useContext(SearchContext);
   const { searchResult } = context;
   const { result, length } = useSearch(searchResult, data);
-  console.log(result);
+  const hideAction = (e) => {
+    setShow(false);
+    console.log(e.target);
+  };
   return (
     <div className="searchResults">
       <div className="desktop">
@@ -25,12 +28,32 @@ export default function Search() {
           <div className="two"></div>
           <div className="three"></div>
         </div>
-
         <div className="right">
           <div className="top">
             <p>
               {searchResult.location}: {length} properties found
             </p>
+            <div>
+              <div
+                className="sort-options"
+                onClick={() => {
+                  setShow(true);
+                }}
+              >
+                <img src={sort} alt="" />
+                <p>Top picks for your search</p>
+              </div>
+              {show && (
+                <div className="hidden-item">
+                  <p onClick={hideAction} value="rer">
+                    Top picks for your search
+                  </p>
+                  <p onClick={hideAction}>Stars (highest first)</p>
+                  <p onClick={hideAction}>Stars (lowest first)</p>
+                  <p onClick={hideAction}>Top reviewed</p>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
