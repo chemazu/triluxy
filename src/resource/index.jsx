141093153ba7be0e693acd047791d3eaeba97fff
MAC <user@MACs-MacBook-Pro.local>
2022-03-12 23:49:04 +0100

    mac

--- src/resource/index.jsx
+++ src/resource/index.jsx
@@ -1,9 +1,21 @@
 import sort from "./sort.svg";
+import calendar from "./calendar.svg";
+import person from "./person.svg";
+import search from "./search.svg";
+import filter from "./filter.svg";
+
+
+
+
 import data from "./hotel.json";
 
 export default function ImportContent() {
   return {
     sort,
     data,
+    calendar,
+    person,
+    search,
+    filter
   };
 }
--- src/view/Search/index.jsx
+++ src/view/Search/index.jsx
@@ -1,8 +1,13 @@
 import React, { useContext, useState } from "react";
 import useSearch from "../../hooks/search-hook";
+import { useInput } from "../../hooks/input-hook";
+import { useNavigate } from "react-router-dom";
+
+
 import { SearchContext } from "../../context/SearchContext";
 import "./style.scss";
 import ImportContent from "../../resource";
+import Banner from "../../components/Banner";
 
 export default function Search() {
   const filterOptions = [
@@ -13,13 +18,52 @@ export default function Search() {
   ];
   const [show, setShow] = useState(false);
   const [filterDetail, setFilterDetail] = useState(filterOptions[0]);
-  const { sort, data } = ImportContent();
+  const { sort, data, person, calendar,search,filter } = ImportContent();
   const context = useContext(SearchContext);
-  const { searchResult } = context;
+  const { searchResult ,setSearchResult} = context;
+
   const { result, length } = useSearch(searchResult, data);
   const hideAction = (e) => {
     setShow(false);
     setFilterDetail(filterOptions[e.target.value]);
+  };
+  const {
+    value: location,
+    change: changeLocation,
+    reset: resetLocation,
+  } = useInput(searchResult.location);
+  const history = useNavigate();
+
+
+  const {
+    value: checkIn,
+    change: changeCheckIn,
+    reset: resetCheckIn,
+  } = useInput("");
+  const {
+    value: checkOut,
+    change: changeCheckOut,
+    reset: resetCheckOut,
+  } = useInput("");
+  const { value: adult, change: changeAdult, reset: resetAdult } = useInput("");
+  const {
+    value: children,
+    change: changeChildren,
+    reset: resetChildren,
+  } = useInput("");
+  const { value: room, change: changeRoom, reset: resetRoom } = useInput("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchResult({ location, checkIn, checkOut, adult, room });
+
+    resetLocation();
+    resetAdult();
+    resetCheckIn();
+    resetCheckOut();
+    resetRoom();
+    resetChildren();
+    history("/search");
   };
 
   return (
@@ -33,7 +77,59 @@ export default function Search() {
             </span>
             <span>Search result : </span>
           </div>
-          <div className="two"></div>
+          <div className="two">
+          <form>
+        <div className="place-search">
+          <div className="search-item">
+            <label>Where are you going ? </label>
+            <input
+              type="text"
+              list="listid"
+              {...changeLocation}
+              placeholder="Where are you going?"
+            />
+            <datalist id="listid">
+              <option label="Popular Destinations" value="&zwnj;" />{" "}
+              {/* write a condition for this , incase someone sends this above empty value */}
+              <option value="Abuja" label="Nigeria" />
+              <option value="Lagos" label="Nigeria" />
+              <option value="Ikeja" label="Nigeria" />
+              <option value="Lekki" label="Nigeria" />
+              <option value="Ibadan" label="Nigeria" />
+            </datalist>
+          </div>
+        </div>
+
+        <div className="date-search">
+          <div className="search-item">
+            <label>Check-in </label>
+            <input type="date" {...changeCheckIn} />
+          </div>
+          <div className="search-item">
+            <label>Check-out </label>
+            <input type="date" {...changeCheckOut} />
+          </div>
+        </div>
+
+        <div className="info-search">
+          <div className="search-item">
+            <label>adults </label>
+            <input type="number" placeholder="adults" {...changeAdult} />
+          </div>
+
+          <div className="search-item">
+            <label>children</label>
+
+            <input type="number" placeholder="children" {...changeChildren} />
+          </div>
+          <div className="search-item">
+            <label>rooms </label>
+
+            <input type="number" placeholder="rooms" {...changeRoom} />
+          </div>
+        </div>
+      </form>
+          </div>
           <div className="three"></div>
         </div>
         <div className="right">
@@ -71,6 +167,49 @@ export default function Search() {
           </div>
         </div>
       </div>
+      {/* <div className="mobile">
+        <div className="search">
+
+            <div className="place-search">
+              <div className="search-item">
+                <img src={search} alt="" />
+                <input
+                  type="text"
+                  list="listid"
+                  {...changeLocation}
+                  placeholder="Where are you going?"
+                />
+                <datalist id="listid">
+                  <option label="Popular Destinations" value="&zwnj;" />{" "}
+                  <option value="Abuja" label="Nigeria" />
+                  <option value="Lagos" label="Nigeria" />
+                  <option value="Ikeja" label="Nigeria" />
+                  <option value="Lekki" label="Nigeria" />
+                  <option value="Ibadan" label="Nigeria" />
+                </datalist>
+
+
+              </div>
+
+
+            </div>
+            <div className="mini-result"><img src={calendar} />
+              <span>{searchResult.checkIn} - {searchResult.checkOut}</span><img src={person} />
+              <span>{searchResult.adult} adults</span></div>
+
+        </div>
+
+        <div className="mini-result">
+          <div>
+          <img src={sort} />
+              <span>Sort</span>
+            </div>
+            <div>
+            <img src={filter} />
+            <span>Filter</span></div>
+            </div>
+              
+      </div> */}
     </div>
   );
 }
